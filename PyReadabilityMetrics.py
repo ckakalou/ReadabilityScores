3c226ba6f3a621301a99b3960c7e546db8cbb090
ckakalou <ckakalou@certh.gr>
2023-08-07 17:15:19 +0300

    Added masking from list

--- PyReadabilityMetrics.py
+++ PyReadabilityMetrics.py
@@ -7,21 +7,14 @@ from readability import Readability
 
 original_text = """"
 
-What is personalised cancer medicine about?
-Personalized medicine against cancer aims to find the best cancer treatment for the particular characteristics of each patient’s tumor. Nowadays, doctors can obtain a tumor sample using modern technology to sequence its DNA, with the aim to identify the specific mutations of the tumor to inform treatment decisions. However, this is not an easy step since a tumor might contain hundreds of mutations.  It is key for the oncologist to know which mutations (usually few) are more likely to play a role in the tumor growth, to use the best anti-cancer therapies that can target these genomic alterations. Therefore, and due to the complexity of the mutations found during the tumor sequencing, defining the mutations involved in tumor growth is still a very challenging and time-consuming process. 
-What if there was a tool to help interpret these cancer mutations?
-We have created the tool Cancer Genome Interpreter or CGI, to help clinicians to make the best treatment decision for the patient. The CGI is part of a project funded by the European Union in which clinicians, oncology researchers and patient association from 6 countries join forces to develop and improve the CGI portal. 
-What is the CGI tool and CGI-Clinics?
-CGI is a tool that analyses mutations occurring in tumors (studied from biopsies) in a safe manner and identifies those cancer mutations that make cancer progress. CGI learns directly from data from previous patients, thus it uses what is called machine learning algorithms. CGI was developed as a research tool by scientists at the Barcelona Biomedical Genomics lab at IRB Barcelona. Now through CGI-Clinics, those scientists together with 17 other European institutions aim at adapting CGI into the clinics to be of use for patients.
-How can patients contribute to CGI?
-In the process of improving the CGI tool, we need information from thousands of tumors already sequenced, so that CGI could combine bioinformatics analysis with databases of clinical knowledge. CGI could then merge all this information and provide an interpretation of the mutations that will help clinicians to choose the best treatment for each patient. 
-In consequence, our goal is to nourish the CGI platform with genomic data of cancer patients, and the first step would be to encourage patients to share their sequencing data. Would you help us? 
-Why are we asking you to participate in this focus group?
-Sequencing data is both complex information and useful to manage cancer treatment and progression. In CGI-Clinics we are well aware of this and would like to help patients cope with the new concepts arising in personalised cancer medicine: next generation sequencing, tumor profiling, machine-learning, variants of unknown significance, etc. We have a team of professionals that thanks to you will identify a set of questions that are important to empower patients and help them be active actors both in conversations with the oncologists and in data-sharing processes with scientists.
-
+Personalized medicine is an approach to find the most effective cancer treatment tailored specifically to your tumor's unique characteristics. Nowadays, doctors use advanced technology to collect a small sample of your tumor and examine its DNA through a process called sequencing. This helps identify specific mutations within the tumor, which are changes in its genetic makeup. Though this step can be complicated because tumors may have many mutations, it's crucial for your oncologist to determine which mutations are most likely driving the tumor's growth. By understanding these key mutations, your doctor can identify the best anti-cancer therapies that target these specific genetic changes. The process of analyzing mutations is challenging and takes time, but it's essential to find the most suitable treatment for you
 
 """
-r = Readability(original_text)
+
+refactored_test = """"
+Personalized medicine means finding the best treatment for your cancer based on how your tumor is different. Doctors use advanced technology to study a tiny piece of your tumor's DNA. They look for specific changes called mutations. Figuring out these changes can be tricky because tumors can have many of them. But it's important for your doctor to find the ones that make your tumor grow. By understanding these changes, your doctor can find the best treatments that target them. Studying these changes is hard and takes time, but it helps find the best treatment for you.
+"""
+r = Readability(refactored_test)
 
 print("Flesch Reading Ease scores are: ", r.flesch())
 print("Flesch - Kincaid scores are: ", r.flesch_kincaid())
--- TextstatPlugin.py
+++ TextstatPlugin.py
@@ -3,33 +3,43 @@
 # Plugin from: https://github.com/textstat/textstat
 
 import textstat
+import regex as re
 
 original_text = """"
+Personalized medicine aims to find the best cancer treatment for the particular characteristics of each patient’s tumor. Nowadays, doctors can obtain a tumor sample using modern technology to sequence its DNA. This helps  to identify the specific mutations of the tumor and informs about treatment decisions. However, this is not an easy step since a tumor might contain hundreds of mutations. Nonetheless, it is key for the oncologist to know which mutations (usually few) are more likely to play a role in the tumor growth. This way, the best anti-cancer therapies that can target these genomic alterations can be identified. Mutations found during the tumor secuencing are complex.  Defining the mutations involved in tumor growth is a very challenging and time-consuming process."""
 
-What is personalised cancer medicine about?
-Personalized medicine against cancer aims to find the best cancer treatment for the particular characteristics of each patient’s tumor. Nowadays, doctors can obtain a tumor sample using modern technology to sequence its DNA, with the aim to identify the specific mutations of the tumor to inform treatment decisions. However, this is not an easy step since a tumor might contain hundreds of mutations.  It is key for the oncologist to know which mutations (usually few) are more likely to play a role in the tumor growth, to use the best anti-cancer therapies that can target these genomic alterations. Therefore, and due to the complexity of the mutations found during the tumor sequencing, defining the mutations involved in tumor growth is still a very challenging and time-consuming process. 
-What if there was a tool to help interpret these cancer mutations?
-We have created the tool Cancer Genome Interpreter or CGI, to help clinicians to make the best treatment decision for the patient. The CGI is part of a project funded by the European Union in which clinicians, oncology researchers and patient association from 6 countries join forces to develop and improve the CGI portal. 
+
+refactored_test = """"
+What is personalised cancer medicine?
+Personalized medicine means finding the best treatment for your cancer based on how your tumor is different. Doctors use advanced technology to study a tiny piece of your tumor's DNA. They look for specific changes called mutations. Figuring out these changes can be tricky because tumors can have many of them. But it's important for your doctor to find the ones that make your tumor grow. By understanding these changes, your doctor can find the best treatments that target them. Studying these changes is hard and takes time, but it helps find the best treatment for you.
+What if there was a tool to help explain and understand these cancer mutations?
+We made a tool called Cancer Genome Interpreter (CGI) to help doctors choose the best treatment for patients. CGI is a part of a project supported by the European Union. In this project, doctors, cancer researchers, and patient groups from 6 countries work together to make the CGI portal better and more helpful.
 What is the CGI tool and CGI-Clinics?
-CGI is a tool that analyses mutations occurring in tumors (studied from biopsies) in a safe manner and identifies those cancer mutations that make cancer progress. CGI learns directly from data from previous patients, thus it uses what is called machine learning algorithms. CGI was developed as a research tool by scientists at the Barcelona Biomedical Genomics lab at IRB Barcelona. Now through CGI-Clinics, those scientists together with 17 other European institutions aim at adapting CGI into the clinics to be of use for patients.
-How can patients contribute to CGI?
-In the process of improving the CGI tool, we need information from thousands of tumors already sequenced, so that CGI could combine bioinformatics analysis with databases of clinical knowledge. CGI could then merge all this information and provide an interpretation of the mutations that will help clinicians to choose the best treatment for each patient. 
-In consequence, our goal is to nourish the CGI platform with genomic data of cancer patients, and the first step would be to encourage patients to share their sequencing data. Would you help us? 
-Why are we asking you to participate in this focus group?
-Sequencing data is both complex information and useful to manage cancer treatment and progression. In CGI-Clinics we are well aware of this and would like to help patients cope with the new concepts arising in personalised cancer medicine: next generation sequencing, tumor profiling, machine-learning, variants of unknown significance, etc. We have a team of professionals that thanks to you will identify a set of questions that are important to empower patients and help them be active actors both in conversations with the oncologists and in data-sharing processes with scientists.
-
-
+CGI is a tool that helps doctors look at changes in tumors (they study small samples from the tumors) in a safe way. It finds the changes (called mutations) that make the cancer grow. CGI learns from the information of other patients, using special technology called machine learning. Scientists at the Barcelona Biomedical Genomics lab at IRB Barcelona created CGI as a research tool. Now, with CGI-Clinics, those scientists and 17 other European institutions want to make CGI useful for patients by using it in clinics.
+How can patients help with CGI?
+To make the CGI tool better, we need information from many tumors that were already studied. CGI will use this information along with medical knowledge to understand the mutations and find the best treatment for each patient. Our aim is to collect information from cancer patients, like their genes and the DNA data of their cancer, for the CGI platform. The first step is to ask patients to share this tumor information called sequencing data. Will you help us with this?
+Why do we want you to be part of this group and share your thoughts?
+Sequencing data is like a puzzle with complicated information, but it can really help with cancer treatment. In CGI-Clinics, we want to help patients understand all these new ideas in personalized cancer medicine, like next-generation sequencing and tumor profiling. We have a team of experts who want to hear from you and ask important questions. This will help patients feel more confident when talking to their doctors and sharing information with scientists.
 """
-print("Flesch Reading Ease score is: ", textstat.flesch_reading_ease(original_text))
-print("Flesch-Kincaid Grade Level score is: ", textstat.flesch_kincaid_grade(original_text))
-print("Gunning Fog Index score is: ", textstat.gunning_fog(original_text))
-print("SMOG Index score is: ", textstat.smog_index(original_text))
-print("Coleman-Liau Index score is: ", textstat.coleman_liau_index(original_text))
-print("Automated Readability Index score is: ", textstat.automated_readability_index(original_text))
-print("Dale-Chall Readability Score score is: ", textstat.dale_chall_readability_score(original_text))
-print("Difficult Words score is: ", textstat.difficult_words(original_text))
-print("Linsear Write Formula score is: ", textstat.linsear_write_formula(original_text))
-print("Gunning Fog score is: ", textstat.gunning_fog(original_text))
-print("Text Standard score is: ", textstat.text_standard(original_text))
-print("Gulpease Index score is: ", textstat.gulpease_index(original_text))
-print("Osman score is: ", textstat.osman(original_text))
+
+
+print("Flesch Reading Ease score is: ", textstat.flesch_reading_ease(refactored_test))
+print("Flesch-Kincaid Grade Level score is: ", textstat.flesch_kincaid_grade(refactored_test))
+print("Gunning Fog Index score is: ", textstat.gunning_fog(refactored_test))
+print("SMOG Index score is: ", textstat.smog_index(refactored_test))
+print("Coleman-Liau Index score is: ", textstat.coleman_liau_index(refactored_test))
+print("Automated Readability Index score is: ", textstat.automated_readability_index(refactored_test))
+print("Dale-Chall Readability Score score is: ", textstat.dale_chall_readability_score(refactored_test))
+print("Difficult Words score is: ", textstat.difficult_words(refactored_test))
+print("Linsear Write Formula score is: ", textstat.linsear_write_formula(refactored_test))
+print("Gunning Fog score is: ", textstat.gunning_fog(refactored_test))
+print("Text Standard score is: ", textstat.text_standard(refactored_test))
+print("Gulpease Index score is: ", textstat.gulpease_index(refactored_test))
+print("Osman score is: ", textstat.osman(refactored_test))
+
+dummy_final_text = refactored_test.replace("tumor", "apple")
+
+difficultWords = ['mutations', 'sequencing', 'cancer', 'tumor', 'tumors', 'treatment', 'mutation', 'mutations', 'Sequencing', 'Genomics', 'Clinics', 'clinics','CGI', 'cgi-clinics', 'scientists', 'personalized', 'personalised', 'Genome', 'genomic', 'genome', 'Interpreter', 'medical', 'clinics', 'lab', 'Lab', 'sequencing', 'researchers']
+big_regex = re.compile('|'.join(map(re.escape, difficultWords)))
+the_message = big_regex.sub("apple",  refactored_test)
+print(the_message)
